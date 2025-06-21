110db0c1c4a5a2ba9b8aeab6a171e1aea84ea1c4
saadaltafofficial <saadbeenco@gmail.com>
2025-06-21 15:37:07 +0500

    Fix formatting of the __tablename__ attribute in the User model

--- app/models.py
+++ app/models.py
@@ -14,7 +14,8 @@ class Post(Base):
 
 
 class User(Base):
-    __tablename__= "users"
+    __tablename__= "users"    
+    
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     username= Column(String, nullable=False, unique=True)
