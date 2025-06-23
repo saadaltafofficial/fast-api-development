d6fc3e6748e6673c66396bebd08d680586ea4610
saadaltafofficial <saadbeenco@gmail.com>
2025-06-24 01:00:27 +0500

    Implement voting functionality with Vote model and routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from .database import engine
 from . import models, config
 from fastapi import FastAPI
-from .routers import posts, users, auth
+from .routers import posts, users, auth, vote
 
 
 
@@ -11,6 +11,7 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Literal
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -47,3 +47,9 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+
+class Vote(BaseModel):
+    post_id:int
+    dir: Literal[0, 1]
